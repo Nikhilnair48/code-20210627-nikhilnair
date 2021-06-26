22fc5197702d4db8c4a395a9becadfe3d3f64a26
Nikhilnair48 <nikhil.nair48@gmail.com>
2021-06-27 01:08:37 +0530

    Added inline comments and exported all functions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Transform, Readable } = require("stream");
 const fs = require("fs");
 const Exception = require("./exceptions/bmi-exception");
 
+// Helper function to determine BMI category
 function determineBMICategory(bmi) {
   let category = "";
   if (bmi < 18.4) category = "Underweight";
@@ -14,6 +15,7 @@ function determineBMICategory(bmi) {
   return category;
 }
 
+// Helper function to determine health risk
 function determineBMIHealthRisk(bmi) {
   try {
     let risk = "";
@@ -29,6 +31,11 @@ function determineBMIHealthRisk(bmi) {
   }
 }
 
+/*
+  findBMI - given an object containing gender, height and weight, determine the BMI, 
+  BMI category and health risk
+  Sample object: { "Gender": "Male", "HeightCm": 171, "WeightKg": 96 }
+*/
 function findBMI(data) {
   try {
     data["BMI"] = (data.WeightKg / Math.pow(data.HeightCm / 100, 2)).toFixed(2);
@@ -40,8 +47,13 @@ function findBMI(data) {
   }
 }
 
+// While handling large streams of data (ex: 20000 JSON objects), the open bracket, ("["), commas separating objects
+// and closed brackets ("]") will have to be added - unless done manually.
+// for the first object, the boolean will permit us to add the open bracket
+// for consequent objects, a comma will be added in the Transform stream
 let firstObjectRead = false;
 
+// Transform stream - given a JSON, invoke findBMI and push the updated chunk onto the stream
 const transform = new Transform({
   readableObjectMode: true,
   writableObjectMode: true,
@@ -56,19 +68,27 @@ const transform = new Transform({
   }
 });
 
+/*
+  processInput - reads the file at 'inputPath', performs a transformation and writes to the file at the 'outputPath' 
+*/
 function processInput(inputPath, outputPath) {
   try {
+    // Init read and write stream
     const readStream = fs.createReadStream(inputPath);
     const writeStream = fs.createWriteStream(outputPath);
+    // Init json parser - particularly necessary for read
     const jsonParser = StreamArray.withParser();
 
+    // Read from file and pipe to the JSON parser
     readStream.pipe(jsonParser.input).on("error", (error) => {
       Exception.error("Error parsing json.", error, "TransformException");
     });
+    // Once the chunks are parsed, pass them to the transform stream, and write the output
     jsonParser.pipe(transform).pipe(writeStream, (error) => {
       Exception.error("Error on write stream.", error, "WriteStreamException");
     });
 
+    // Once the writes are done, ensure to add the closing bracket for the list of objects
     writeStream.on("finish", function () {
       fs.appendFileSync(outputPath, "]");
     });
@@ -77,9 +97,12 @@ function processInput(inputPath, outputPath) {
   }
 }
 
+// Process all the data from the first argument and write to the file in the second argument
 processInput("./data/sample1.json", "./data/sample2.json");
 
 module.exports = {
   processInput: processInput,
-  findBMI: findBMI
+  findBMI: findBMI,
+  determineBMICategory: determineBMICategory,
+  determineBMIHealthRisk: determineBMIHealthRisk
 };
