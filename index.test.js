27e56c017edf22692d06f7d1774e418e87752a7d
Nikhilnair48 <nikhil.nair48@gmail.com>
2021-07-04 23:36:21 +0530

    Testing processInput with large dataset updated

--- index.test.js
+++ index.test.js
@@ -19,31 +19,24 @@ async function populateData(largeDataInputPath) {
   let fileExists = await fs.existsSync(largeDataInputPath);
   // Only generate data if the file doesn't exist
   if (!fileExists) {
-    await fs.appendFileSync(largeDataInputPath, "[");
-
     // Generate a large set of data
     const gender = ["Male", "Female"];
-    for (let i = 0; i < 20000; i++) {
+    for (let i = 0; i < 15; i++) {
       let data = JSON.stringify({
         Gender: gender[parseInt(Math.random() * 2)],
         HeightCm: parseInt(Math.random() * (200 - 80) + 80),
         WeightKg: parseInt(Math.random() * (250 - 5) + 5)
       });
-      if (i == 19999) data += "";
-      else data += ",";
-
       // Write to output file
-      await fs.appendFileSync(largeDataInputPath, data);
+      await fs.appendFileSync(largeDataInputPath, data + "\n");
     }
-    await fs.appendFileSync(largeDataInputPath, "]");
   }
 }
 
 test("Compute BMI with large, generated dataset", async () => {
-  const largeDataInputPath = "./data/sample3.json";
+  const largeDataInputPath = "./data/sample7.txt";
   await populateData(largeDataInputPath);
-  const res = await processInput(largeDataInputPath, "./data/sample4.json");
-
+  const res = await processInput(largeDataInputPath, "./data/sample8.txt");
   // To do: validate at least one object in the output to ensure all went fine
   expect(res).toEqual({ status: "Success" });
-});
+}, 30000);
